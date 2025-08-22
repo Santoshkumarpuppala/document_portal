037ac38d79533044e0238c2bb4e8879ca2c2bd89
Santosh <puppalas97@gmail.com>
2025-08-21 21:39:24 -0400

    custom exception class

---
+++ logger/custom_logger.py
@@ -0,0 +1,29 @@
+import logging
+import os
+from datetime import datetime
+
+class CustomLogger:
+    def __init__(self, log_dir="logs"):
+        # Ensure Logs Directory Exists
+        self.logs_dir = os.path.join(os.getcwd(), log_dir)
+        os.makedirs(self.logs_dir, exist_ok=True)
+
+        # Create timestamped log file name
+        log_file = f"{datetime.now().strftime('%m_%d_%Y_%H_%M_%S')}.log"
+        self.log_file_path = os.path.join(self.logs_dir, log_file)
+
+        # Configure logging
+        logging.basicConfig(
+            filename=self.log_file_path,
+            format="[ %(asctime)s ] %(levelname)s %(name)s (line:%(lineno)d) - %(message)s",
+            level=logging.INFO,
+        )
+        print(f"Logging to: {self.log_file_path}")
+
+    def get_logger(self, name=__file__):
+        return logging.getLogger(os.path.basename(name))
+
+if __name__ == "__main__":
+    logger = CustomLogger()
+    log = logger.get_logger(__file__)
+    log.info("Custom logger initialized")
