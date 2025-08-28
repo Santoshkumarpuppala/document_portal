a67783aff9f2bb5c4b34d3e055dbada56b3ad716
Santosh <puppalas97@gmail.com>
2025-08-27 21:38:46 -0400

    prompt library

--- model/models.py
+++ model/models.py
@@ -1,5 +1,17 @@
 from pydantic import BaseModel, Field
-from typing import Optional, List, Dict, Any
+from typing import List, Union
 
 
+
+class Metadata(BaseModel):
+    Summary: List[str]
+    Title: str
+    Author: str
+    DateCreated: str
+    LastModifiedDate: str
+    Publisher: str
+    Language: str
+    PageCount: Union[int, str]
+    SentimentTone: str
+    
 
